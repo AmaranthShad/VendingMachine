ce7396805d53ac7f707661c753ed69bc752c63ec
Emily Quiles <emilyquiles1@gmail.com>
2023-07-27 20:01:27 -0400

    Pushing psuedo code

--- src/main/java/com/techelevator/PurchaseMenu.java
+++ src/main/java/com/techelevator/PurchaseMenu.java
@@ -2,5 +2,36 @@ package com.techelevator;
 
 public class PurchaseMenu {
 
-    //choice 2, displays current money $0.0 and asks them to feed in money, then what item they would like to purchase
+   public something purchaseMenuChoice(){
+       double currentMoney = 0.0;
+       System.out.println(currentMoney);
+       System.out.println("(1) Feed Money");
+       System.out.println("(2) Select Product");
+       System.out.println("(3) Finish Transaction");
+       String purchaseMenuOption = in.nextLine();
+       if(purchaseMenuOption = "1"){
+           System.out.println(currentMoney);
+           System.out.println("Please feed money: ");
+           double moneyAdded = scanner.in;
+           currentMoney += moneyAdded;
+           System.out.println(currentMoney);
+       }
+       if(purchaseMenuOption = "2"){
+           productChoice();
+       }
+
+   }
+
+   public object productChoice(String slot) {
+    displayAllItems();
+    String slot = in.nextLine();
+       if (slot exists){
+           System.out.println("The selection you made: " + item.getName() + " costs " + item.getCost() + "There are " + item.getQuantity() + "remaining in stock.");
+       }
+    else{
+           System.out.println("The selection you entered does not exist.");
+           return to purchase menu;
+       }
+   }
+
 }
