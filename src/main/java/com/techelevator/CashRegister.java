f3452db6f275972c5b57d9c0ae432e4e169ff364
Shadav Khan <shadavhkhan@gmail.com>
2023-07-28 11:14:54 -0400

    Implemented CashRegister

---
+++ src/main/java/com/techelevator/CashRegister.java
@@ -0,0 +1,51 @@
+package com.techelevator;
+
+import java.text.NumberFormat;
+
+public class CashRegister {
+    private double balance;
+
+    public CashRegister() {
+        balance = 0.0;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+    public String getFormattedBalance() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        return formatter.format(balance);
+    }
+    public void addToBalance(double money){
+        if (money< 0.0){
+            System.out.println("You can't trick me with your negative money");
+            return;
+        }
+        balance += money;
+    }
+    public void getChange(){
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
+        while (balance > 0.25){
+            balance -= 0.25;
+            quarters++;
+        }
+        while (balance > 0.10){
+            balance -= 0.10;
+            dimes++;
+        }
+        while (balance > 0.05){
+            balance -= 0.05;
+            nickels++;
+        }
+        System.out.print("Your change is: ");
+        if (quarters>0)
+            System.out.print(quarters + " quarters");
+        if (dimes>0)
+            System.out.print(dimes + " dimes");
+        if (nickels>0)
+            System.out.print(nickels + " nickels");
+        balance = 0;
+    }
+}
