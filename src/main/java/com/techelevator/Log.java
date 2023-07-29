d547a333dcb8cf7c3e6ca15230f2125315616cca
Shadav Khan <shadavhkhan@gmail.com>
2023-07-29 17:57:55 -0400

    Created a generateSalesReport method

--- src/main/java/com/techelevator/Log.java
+++ src/main/java/com/techelevator/Log.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Scanner;
+import java.util.Set;
 
 import static java.lang.System.exit;
 
@@ -28,9 +30,35 @@ public class Log {
         }
     }
 
+    public static void generateSalesReport(Set<VendingItem> index){
+        String ld = String.valueOf(LocalDate.now());
+        String lt = String.valueOf(LocalTime.now());
+        lt = lt.substring(0,lt.lastIndexOf(".")).replaceAll(":", "-");
+        File salesReport = new File(ld+"_"+lt+"salesReport.txt");
+        try(PrintWriter writer = new PrintWriter(salesReport)){
+            double totalSales = 0;
+            for (VendingItem vendingItem : index) {
+                String name = vendingItem.getName();
+                int regularPurchases = vendingItem.getRegularPurchase();
+                int discountedPurchases = vendingItem.getDiscountedPurchase();
+                String message = name + "|" +
+                        regularPurchases + "|" +
+                        discountedPurchases;
+                writer.println(message);
+                double revenue = regularPurchases+discountedPurchases;
+                revenue *= vendingItem.getCost();
+                revenue -= discountedPurchases;
+                totalSales += revenue;
+            }
+            String revenue = NumberFormat.getCurrencyInstance().format(totalSales);
+            writer.println("\r\n**TOTAL SALES** " + revenue);
+        }catch (FileNotFoundException e){
+            System.out.println("SalesReport file not found for writing");
+        }
+    }
+
     public void printNext(String newLine){
-        File temp = null;
-        temp = new File("tmp");
+        File temp = new File("tmp");
         try (PrintWriter output = new PrintWriter(temp)) {
             if (!log.exists()) {
                 output.println(newLine);
